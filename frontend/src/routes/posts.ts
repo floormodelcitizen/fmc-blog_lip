2a4edc1a227d902306e00f95e1d720270763b4cb
Chad Christopher <devluxv20@gmail.com>
2022-07-15 22:25:42 -0700

    Fetch Blog posts

--- frontend/src/routes/posts.ts
+++ frontend/src/routes/posts.ts
@@ -1,1 +1,7 @@
-import type { EndpointOutput } from '@sveltejs/kit';
+import type { RequestHandlerOutput } from '@sveltejs/kit';
+
+export async function get(): Promise<RequestHandlerOutput> {
+	const res = await fetch('http://localhost:1337/api/posts?populate=*');
+	const data = await res.json();
+	return { body: data };
+}
